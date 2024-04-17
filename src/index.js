3fdce8efaf3dcaa122708777077e14bfbfcb3ef5
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-04-17 08:55:17 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+window.addEventListener("load", () => {
+  fetch(imgUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const dogImages = data.message;
+      dogImages.forEach((image) => {
+        const img = document.createElement("img");
+        img.src = image;
+        document.getElementById("dog-image-container").appendChild(img);
+      });
+    });
+});
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+window.addEventListener("load", () => {
+  fetch(breedUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const breeds = Object.keys(data.message);
+      breeds.forEach((breed) => {
+        const li = document.createElement("li");
+        li.textContent = breed;
+        document.getElementById("dog-breeds").appendChild(li);
+      });
+    });
+});
+
+const dogBreeds = document.getElementById("dog-breeds");
+
+dogBreeds.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.style.color = "red";
+  }
+});
